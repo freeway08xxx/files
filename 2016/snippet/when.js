781442aa251e253be1e2b3e881aecd24e6c809fd
freeway08xxx <freeway08xxx@yahoo.co.jp>
2017-07-12 14:51:14 +0900

    rリファクタリング

--- 2016/snippet/when.js
+++ 2016/snippet/when.js
@@ -1,34 +1,35 @@
-        //都道府県、市区町村リストを取得
-        var monitor = []; //データ取得終了を監視
-        var get;
-        var q = [
-            {grpby : 'ken'}
-            ,{grpby : 'city'}
-        ]
-        $.each(q, function(i, query){
-            get = service.getData('count', query)
-                .then(function(data, type){
-                    var d = $.Deferred();
-                    serviceModel[type].catchError(data)
-                        .then(function(count){
-                            serviceModel.count.storage[query.grpby] = count;
-                            d.resolve();
-                        }
-                        ,function(errorStatus, errorCode){
-                            view.errorAppend(type, errorStatus, errorCode, model.$wrapper);
-                            model.$loading.hide();
-                            model.$wrapper.show();
-                            d.reject();
-                        });
-                    return d.promise();
+// jQuery 非同期の繰り返し処理
+var monitor = []; //データ取得終了を監視
+var get;
+var q = [
+    {grpby : 'ken'},
+    {grpby : 'city'}
+]
+
+$.each(q, function(i, query){
+    get = service.getData('count', query)
+        .then(function(data, type){
+            var d = $.Deferred();
+            serviceModel[type].catchError(data)
+                .then(function(count){
+                    serviceModel.count.storage[query.grpby] = count;
+                    d.resolve();
+                }
+                ,function(errorStatus, errorCode){
+                    view.errorAppend(type, errorStatus, errorCode, model.$wrapper);
+                    model.$loading.hide();
+                    model.$wrapper.show();
+                    d.reject();
                 });
-            monitor.push(get);
-        })
-        //データ取得終了後に実行
-        $.when.apply($, monitor).done(function() {
-            view.createPrefList();
-            view.createCityList();
-            view.setCondListAction();
-            model.$loading.hide();
-            model.$wrapper.show();
-        })
+            return d.promise();
+        });
+    monitor.push(get);
+})
+//データ取得終了後に実行
+$.when.apply($, monitor).done(function() {
+    view.createPrefList();
+    view.createCityList();
+    view.setCondListAction();
+    model.$loading.hide();
+    model.$wrapper.show();
+})
